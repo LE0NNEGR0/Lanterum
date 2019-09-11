ab3d8f77c775a0a3687efeb5581a244fc092555a
Sayabedra Santiago <sayabedra.s@gmail.com>
2019-09-11 20:59:17 -0300

    nuevas modificaciones

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 var ventana = window.innerWidth;
 /*alert("el ancho de la ventana es "+ ventana);*/
-
+var menu = document.getElementById("Menu");
+menu.style.marginTop="-63%";
 var hsmenu = document.getElementById('h-s');
 hsmenu.addEventListener("click",function(){
-var menu = document.getElementById("Menu");
+
+	
 	
 
-	if(menu.style.marginTop=="550px")
+	if(menu.style.marginTop=="-63%")
 	{
-		menu.style.marginTop= "15px";
+		menu.style.marginTop= "60%";
 		menu.style.transition="0.3s";
 
 	}
 	else
 	{
-		menu.style.marginTop="550px";
+		menu.style.marginTop="-63%";
 	menu.style.zIndex="20";
 	menu.style.transition="0.3s";
 	}
@@ -23,20 +25,20 @@ var menu = document.getElementById("Menu");
 });
 
 var redes = document.getElementById('redes');
-redes.style.bottom="-110px";
+redes.style.bottom="-100px";
 redes.addEventListener("click",function(){
 
 
-	if(redes.style.bottom=="-110px")
+	if(redes.style.bottom=="-100px")
 	{
 
-		redes.style.bottom= "10px";
+		redes.style.bottom= "100px";
 		redes.style.transition="0.3s";
 		redes.style.zIndex="19";
 	}
 	else
 	{
-		redes.style.bottom="-110px";
+		redes.style.bottom="-100px";
 		redes.style.zIndex="2";
 		redes.style.transition="0.3s";
 	}
